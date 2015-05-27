dff31ff726e4eb57f2d7459d05fb0f92e88bf9b8
Ionică Bizău <bizauionica@yahoo.com>
2015-05-27 21:07:37 +0300

    Added the template file

---
+++ src/index.templ.js
@@ -0,0 +1,21 @@
+// Dependencies
+var Fs = require("fs")
+  , Barbe = require("barbe")
+  ;
+
+/**
+ * KindlyLicense
+ * Generates a version of the KINDLY license providing the year and the author.
+ *
+ * @name exports
+ * @function
+ * @param {String} year The copyright year.
+ * @param {String} author The copyright holder.
+ * @return {String} The KINDLY License generated for provided data.
+ */
+module.exports = function (year, author) {
+    return Barbe("{license}", ["<", ">"], {
+        year: year
+      , author: author
+    });
+};
