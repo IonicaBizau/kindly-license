a6efdc21cab1892b9997b12fbe1a74ffac2a14eb
Ionică Bizău <bizauionica@yahoo.com>
2015-05-27 21:14:50 +0300

    Generate the HTML file

--- build/index.js
+++ build/index.js
@@ -4,10 +4,16 @@ var Barbe = require("barbe")
 
 const KINDLY = Fs.readFileSync(__dirname + "/../LICENSE.kindly", "utf-8")
     , SRC_PATH = __dirname + "/../src/index.templ.js"
+    , HTML_PATH = __dirname + "/../index.templ.html"
+    , HTML = Fs.readFileSync(HTML_PATH, "utf-8")
     , SRC = Fs.readFileSync(SRC_PATH, "utf-8")
     ;
 
 Fs.writeFileSync(SRC_PATH.replace("templ.", ""), Barbe(SRC, {
     license: KINDLY.replace(/\n/g, "\\n")
+}));
+
+Fs.writeFileSync(HTML_PATH.replace("templ.", ""), Barbe(HTML, {
+    license: KINDLY
 }));
 
