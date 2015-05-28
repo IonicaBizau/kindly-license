f98a7ecf525e7390a5404bc60461e7352ad56764
Ionică Bizău <bizauionica@gmail.com>
2015-05-28 09:58:49 +0300

    Updated the lib paths

--- build/index.js
+++ build/index.js
@@ -1,18 +1,22 @@
+// Dependencies
 var Barbe = require("barbe")
   , Fs = require("fs")
   ;
 
+// Constants
 const KINDLY = Fs.readFileSync(__dirname + "/../LICENSE.kindly", "utf-8")
-    , SRC_PATH = __dirname + "/../src/index.templ.js"
+    , LIB_PATH = __dirname + "/../lib/index.templ.js"
     , HTML_PATH = __dirname + "/../index.templ.html"
     , HTML = Fs.readFileSync(HTML_PATH, "utf-8")
-    , SRC = Fs.readFileSync(SRC_PATH, "utf-8")
+    , LIB = Fs.readFileSync(LIB_PATH, "utf-8")
     ;
 
-Fs.writeFileSync(SRC_PATH.replace("templ.", ""), Barbe(SRC, {
+// Create the lib file
+Fs.writeFileSync(LIB_PATH.replace("templ.", ""), Barbe(LIB, {
     license: KINDLY.replace(/\n/g, "\\n")
 }));
 
+// Create the html file
 Fs.writeFileSync(HTML_PATH.replace("templ.", ""), Barbe(HTML, {
     license: KINDLY.replace(/</g, "&lt;").replace(/>/g, "&gt;")
 }));
